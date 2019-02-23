2c99577cfef1a75d39015d6ecbb06fef4d5f9315
DemonV4 <jonatadss@hotmail.com>
2019-02-23 20:08:50 -0300

    Adicionei a classe Usuário

---
+++ src/models/Usuario.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String tipoSangue;
+    private double peso;
+    private double altura;
+    private String email;
+    private char sexo;
+}
